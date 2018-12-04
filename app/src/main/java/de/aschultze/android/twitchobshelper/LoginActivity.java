753f5400636a79802b0e79e6215d6eaf9bce9ebb
dump66 <andyschultze@hotmail.com>
2018-12-04 17:05:30 +0100

    simplified code (but made it less modular)

--- app/src/main/java/de/aschultze/android/twitchobshelper/LoginActivity.java
+++ app/src/main/java/de/aschultze/android/twitchobshelper/LoginActivity.java
@@ -1,18 +1,44 @@
 package de.aschultze.android.twitchobshelper;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends SingleFragmentActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    public static Intent newIntent(Context context) {
-        Intent intent = new Intent(context, LoginActivity.class);
-        return intent;
-    }
+    private static final String TAG = "LoginActivity";
+
+    private ProgressBar mProgress;
+    private EditText mEditUser;
+    private EditText mEditToken;
+    private Button mLogin;
 
     @Override
-    protected Fragment createFragment() {
-        return new LoginFragment();
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_login);
+
+        mProgress = findViewById(R.id.login_pg);
+        mEditUser = findViewById(R.id.login_pt_user);
+        mEditToken = findViewById(R.id.login_pw_token);
+        mLogin = findViewById(R.id.login_btn_login);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mEditUser.getText().toString().equals("") || mEditToken.getText().toString().equals("")) {
+                    Toast.makeText(LoginActivity.this, R.string.login_error, Toast.LENGTH_LONG).show();
+                } else {
+                    // TODO: Twitch Abfrage!
+                }
+            }
+        });
     }
 }
